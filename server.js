24f9b928f017502b175ba60b2daf25ab54374cae
MOA -CODES <moyo.ajla@gmail.com>
2022-09-26 00:04:53 +0100

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+dotenv.config({path:'config.env'}) //configuring env path
+const PORT = process.env.PORT||8080
+
+//log requests
+app.use(morgan('tiny'))
+
+//parse request to body parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine", "ejs") //specifying view engine can be ejs or html or pug
+//app.set("views",path.resolve(__dirname,"views")) 
+
+//load assests
+app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
+
+
+app.get('/', (req,res)=>{
+    // res.send("Crud Application");
+    res.render('index');
+})
+
+app.listen(PORT, ()=>{console.log(`Server is running on http://localhost:${PORT}`)});
