24da2f1612579a1b28d93c2eec06c2f29dc57fe3
MOA -CODES <moyo.ajla@gmail.com>
2022-09-26 00:05:12 +0100

    init

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ejs = require('ejs');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
@@ -15,19 +16,34 @@ app.use(morgan('tiny'))
 //parse request to body parser
 app.use(bodyparser.urlencoded({extended:true}))
 
+
 //set view engine
-app.set("view engine", "ejs") //specifying view engine can be ejs or html or pug
+app.set("view engine", "ejs") 
+//specifying view engine can be ejs or html or pug
 //app.set("views",path.resolve(__dirname,"views")) 
 
 //load assests
-app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
-app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
-app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
+app.use('/css', express.static(path.resolve(__dirname,"assests/css")));
+
+//app.use('/css', express.static(path.resolve(__)))
+app.use('/img', express.static(path.resolve(__dirname,"assests/img")));
+app.use('/js', express.static(path.resolve(__dirname,"assests/js")));
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req,res)=>{ //i believe this means / should open index.ejs
     // res.send("Crud Application");
     res.render('index');
-})
+}) 
+
+app.get('/add-user', (req,res)=>{ //i believe this means /add-user should open add_user.ejs
+    // res.send("Crud Application");
+    res.render('add_user');
+}) 
+
+
+app.get('/update-user', (req,res)=>{ //i believe this means /add-user should open add_user.ejs
+    // res.send("Crud Application");
+    res.render('update_user');
+}) 
 
 app.listen(PORT, ()=>{console.log(`Server is running on http://localhost:${PORT}`)});
