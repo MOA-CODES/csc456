103f3544d5592461076333657b4acf5af98e50f9
MOA -CODES <moyo.ajla@gmail.com>
2022-12-04 00:12:22 +0100

    Q

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try{
         //mongodb connection string
-        const con = await mongoose.connect("mongodb+srv://admin:admin123@cluster0.1zjihei.mongodb.net/CSC456project?retryWrites=true&w=majority")
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+ })
 
         console.log(`MongoDB connected:${con.connection.host}`);
     }catch(err){
